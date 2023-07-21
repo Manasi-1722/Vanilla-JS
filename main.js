79b13d7f127be4aaeefdfd1fb0a26b1c6aa1ff85
Manasi-1722 <88338443+Manasi-1722@users.noreply.github.com>
2023-07-21 22:17:05 +0530

    ex.03

---
+++ main.js
@@ -0,0 +1,16 @@
+// console.log("Hello, script is working in different file!");
+
+// // input
+// var filename = prompt("Give me the file name")          // prompt is used to get data / input from user
+
+// // processing
+// var status = "Successfully downloaded the file " + filename;
+
+// // output 
+// alert(status)  // alert is used to give user data / output     
+
+var btnTranslate = document.querySelector("#btn-translate");
+
+btnTranslate.addEventListener("click", function clickEventHandler(){
+    console.log("clicked!")
+})
